e8b05cd27cd7e8880e452cfad3f82d568f67bdbb
Siddharth Diwan <sidwan02@gmail.com>
2024-04-09 18:00:29 -0400

    sample data for vert osc recorded using pkl dumping

--- vertical_oscillation/log_positions.py
+++ vertical_oscillation/log_positions.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 import json
 from tf2_msgs.msg import TFMessage
-
+import pickle
 
 # from std_msgs.msg import String
 
@@ -20,14 +20,21 @@ class MinimalSubscriber(Node):
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg)
-        msg_json = json.load(msg)
-        print(msg_json)
+        # msg_json = json.load(msg)
+        # print(msg_json)
+
+        afile = open("log_positions.pkl", "ab+")
+        pickle.dump(msg, afile)
+        afile.close()
 
 
 # TODO: figure out how to store this
 
 
 def main(args=None):
+    afile = open("log_positions.pkl", "wb")
+    afile.close()
+
     rclpy.init(args=args)
 
     minimal_subscriber = MinimalSubscriber()
@@ -41,5 +48,24 @@ def main(args=None):
     rclpy.shutdown()
 
 
+def load_and_print_pickle():
+    msgs = []
+    try:
+        afile = open("log_positions.pkl", "rb")
+        while 1:
+            try:
+                msg = pickle.load(afile)
+                msgs.append(msg)
+            except EOFError:
+                break
+        afile.close()
+    except FileNotFoundError:
+        print("Pickle file not found.")
+
+    print(msgs)
+    print(len(msgs))
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    load_and_print_pickle()
