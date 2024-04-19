f5a6bf38ed7424601fb6c3570efff02d6f8e9194
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2024-04-18 20:20:18 -0400

    reading in drone positions -> semitone deltas

--- pydub_test.py
+++ pydub_test.py
@@ -5,9 +5,13 @@
 from log_positions.load_pos import load_pickle
 from pitch_change.find_freq import wav_to_midi
 
+from functools import partial
+
 import random
 import numpy as np
 from pydub import AudioSegment
+
+from collections import defaultdict, deque
 
 import simpleaudio as sa
 import time
@@ -104,36 +108,121 @@ length_so_far = 0
 prev_sound_len = 0
 
 
-for _ in range(30):
-    sound = random.choice(sounds_normalized)
+def allocate_sounds():
+    drone_names = set()
+    for msg in msgs:
+        drone_name = msg[0]
 
-    semitone_delta = random.choice(scale_configs[scale])
+        if drone_name in drone_names:
+            break
 
-    # proportion of length
-    length_prop = np.random.uniform(0.5, 1)
+        drone_names.add(drone_name)
 
-    # song can't be more than 3 seconds
-    length = min(len(sound), 3000) * length_prop
+    drone_sounds = {}
+    available_sounds = list(sounds_normalized)
+    random.shuffle(available_sounds)
 
-    hipitch_sound = change_semitones(sound, semitone_delta, length)
-    # https://stackoverflow.com/questions/43406129/python-overlay-more-than-3-wav-files-end-to-end
+    for drone_name in drone_names:
+        drone_sounds[drone_name] = available_sounds.pop()
 
-    delay_prop = np.random.uniform(0.5, 1)
+    return drone_sounds
 
-    # start the next sound 3 seconds before the previous sound ends
-    insert_time = length_so_far - prev_sound_len * delay_prop
-    # print("baba", insert_time)
-    final_wav = final_wav.overlay(hipitch_sound, position=insert_time)
 
-    # print(len(hipitch_sound))
-    prev_sound_len = len(hipitch_sound)
-    # you might overlay a sound that extends the current length, or it might be within the current length if it's short
-    length_so_far = max(length_so_far, prev_sound_len + insert_time)
-    # print(length_so_far)
-    # print(length_so_far + insert_time)
+drone_sounds = allocate_sounds()
+print("drone sounds: ", drone_sounds)
 
-    print(f"inserted sound of duration {prev_sound_len} at position {insert_time}")
-    print(f"new total length: {length_so_far}")
+# raise Exception("stop here")
+
+
+class Queue:
+    def __init__(self, maxlen=20):
+        self.q = deque()
+        self.maxlen = maxlen
+
+    def append(self, item):
+        if len(self.q) == self.maxlen:
+            self.q.popleft()
+        self.q.append(item)
+
+    def get_std(self):
+        return np.std(self.q)
+
+    def get_mean(self):
+        return np.mean(self.q)
+
+    def __str__(self):
+        return str(self.q)
+
+
+def metric_1(arr, pivot):
+    return np.mean(np.array(arr) - pivot)
+
+
+drone_buffers = defaultdict(lambda: defaultdict(partial(Queue, maxlen=30)))
+temp_buffers = defaultdict(lambda: defaultdict(deque))
+
+start_sec = None
+
+for msg in msgs:
+    # print(msg)
+    drone_name, sec, nsec, x, y, z = msg
+
+    if start_sec is None:
+        start_sec = sec
+
+    temp_buffers[drone_name]["x_temp"].append(x)
+    temp_buffers[drone_name]["y_temp"].append(y)
+    temp_buffers[drone_name]["z_temp"].append(z)
+
+    if sec - start_sec >= 1:
+        start_sec = sec
+
+        err = [
+            metric_1(temp_buffers[drone_name][temp], temp_buffers[drone_name][temp][0])
+            for temp in ["x_temp", "y_temp", "z_temp"]
+        ]
+
+        # TODO: make use of these buffers at some point.
+        drone_buffers[drone_name]["x"].append(err[0])
+        drone_buffers[drone_name]["y"].append(err[1])
+        drone_buffers[drone_name]["z"].append(err[2])
+
+        temp_buffers[drone_name]["x_temp"] = deque()
+        temp_buffers[drone_name]["y_temp"] = deque()
+        temp_buffers[drone_name]["z_temp"] = deque()
+
+        sound = drone_sounds[drone_name]
+
+        print(err)
+        # semitone_delta = random.choice(scale_configs[scale])
+        semitone_delta = np.mean(err) * 120
+        print("semitone delta: ", semitone_delta)
+
+        # proportion of length
+        length_prop = np.random.uniform(0.5, 1)
+
+        # song can't be more than 3 seconds
+        length = min(len(sound), 3000) * length_prop
+
+        hipitch_sound = change_semitones(sound, semitone_delta, length)
+        # https://stackoverflow.com/questions/43406129/python-overlay-more-than-3-wav-files-end-to-end
+
+        delay_prop = np.random.uniform(0.5, 1)
+
+        # start the next sound 3 seconds before the previous sound ends
+        insert_time = length_so_far - prev_sound_len * delay_prop
+        # print("baba", insert_time)
+        final_wav = final_wav.overlay(hipitch_sound, position=insert_time)
+
+        # print(len(hipitch_sound))
+        prev_sound_len = len(hipitch_sound)
+        # you might overlay a sound that extends the current length, or it might be within the current length if it's short
+        length_so_far = max(length_so_far, prev_sound_len + insert_time)
+        # print(length_so_far)
+        # print(length_so_far + insert_time)
+
+        print(f"inserted sound of duration {prev_sound_len} at position {insert_time}")
+        print(f"new total length: {length_so_far}")
 
 final_wav.export("mixed_sounds.wav", format="wav")
 
