44bd89e51ebca4da539cc92c7484478589d507c4
jfan4926 <113312103+jfan4926@users.noreply.github.com>
2024-04-22 12:58:38 -0400

    Add files via upload

---
+++ vertical_four/group6_node.py
@@ -0,0 +1,106 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+from crazyflie_py import generate_trajectory
+import numpy as np
+from blocklyTranslations import *
+from types import SimpleNamespace
+from TimeHelper import TimeHelper # TODO add to files downloaded
+Hz = 30
+
+class worker_node(Node):
+
+    def __init__(self, crazyflies, id=0, num_nodes=1):
+        """
+        id: a unique id between 0 and num_nodes corresponding to the thread number of this worker
+        num_nodes: number of nodes (threads) in total
+        """
+        super().__init__("worker_node_{}".format(id))
+        assert(isinstance(id, int))
+        self.id = id
+        self.num_nodes = num_nodes
+        self.crazyflies = crazyflies
+
+        self.timer = self.create_timer(1/Hz, self.timer_callback)
+        self.timeHelper = TimeHelper(self)
+        self.running = False
+        self.done = False
+    
+    def compute_trajectories(self):
+        """
+        Inject Trajectory computation code here...
+        """
+        trajectories = []
+        ### -----Insert Trajectories Here-------
+
+        return trajectories
+
+    def upload_trajectories(crazyflies, trajectories):
+        '''
+            Upload trajectories to crazyflies one by one
+        '''
+
+        for i, traj in enumerate(trajectories):
+            for cf in crazyflies:
+                cf.uploadTrajectory(traj, i, 0)
+
+    def start(self):
+        """
+            Start execution of blocks
+        """
+        trajectories = self.compute_trajectories()
+        self.upload_trajectories(trajectories)
+        self.execute_blocks()
+
+    def time(self):
+        return self.get_clock().now().nanoseconds / 1e9
+    
+    def execute_blocks(self):
+        """
+        Must be injected into...
+
+        Typical format should be:
+        
+        start_time = 0.0
+        self.timeHelper.sleepUntil(start_time)
+        takeoff(crazyflies, height=1.0, duration=2.0)
+
+        start_time = 5.0
+        self.wait_until(start_time)
+        land(crazyflies, height=0.0, duration=2.0)
+        
+        ...
+
+        Where a new start time is added for each block.
+        """
+        groupState = SimpleNamespace(crazyflies=self.crazyflies, timeHelper=self.timeHelper)
+        ### ---------Insert Execution Code Here------------
+        # Block Name: takeoff high
+        start_time = 0.030000000000000027
+        self.timeHelper.sleepUntil(start_time)
+        takeoff(groupState, 1.6, 3)
+        goto_velocity_relative_position(groupState, -0.5,0,0,0.25)
+        # Block Name: vertical
+        start_time = 5.633332519531254
+        self.timeHelper.sleepUntil(start_time)
+        goto_velocity_relative_position(groupState, 0,0,-0.1,0.25)
+        goto_velocity_relative_position(groupState, 0,0,0.1,0.25)
+        goto_velocity_relative_position(groupState, 0,0,-0.2,0.25)
+        goto_velocity_relative_position(groupState, 0,0,0.2,0.25)
+        goto_velocity_relative_position(groupState, 0,0,-0.3,0.25)
+        goto_velocity_relative_position(groupState, 0,0,0.3,0.25)
+        goto_velocity_relative_position(groupState, 0,0,-0.4,0.25)
+        goto_velocity_relative_position(groupState, 0,0,0.4,0.25)
+        # Block Name: landhigh
+        start_time = 17.02
+        self.timeHelper.sleepUntil(start_time)
+        goto_velocity_relative_position(groupState, 0.5,0,0,0.25)
+        land(groupState, 0,3)
+
+        
+        self.done = True
+    
+    def timer_callback(self):
+        if not self.running:
+            self.start()
+            self.running = True
---
+++ vertical_four/launch_nodes.py
@@ -0,0 +1,95 @@
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from crazyflie_py import Crazyswarm
+import rclpy
+import threading
+
+# Inject Imports Here:
+
+def launch(nodes):
+    executor = rclpy.executors.MultiThreadedExecutor()
+
+    for node in nodes:
+        executor.add_node(node)
+    
+    thread = threading.Thread(target=executor.spin, daemon=True)
+    thread.start()
+    try:
+        while rclpy.ok():
+            pass
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
+    thread.join()
+    
+
+def main():
+    # node_config_file = get_package_share_directory('crazyflie'), 'config', 'crazyflies.yaml'
+    
+    # Initialize swarm
+    swarm = Crazyswarm()
+    with open("cfs_ordering.yaml") as f:
+        ordering = yaml.safe_load(f) #TODO!!!
+        order = ordering['cfs']
+    
+    crazyflies = [swarm.allcfs.crazyfliesById[int(k)] for k in order]
+    counter = 0
+    nodes = []
+    cfs = []
+
+    #   -----------Insert Nodes Here----------- 
+    import group1_node
+    cfs = []
+    cfs.append(crazyflies[0])
+    nodes.append(group1_node.worker_node(cfs, len(nodes), 2))
+
+
+    import group2_node
+    cfs = []
+    cfs.append(crazyflies[1])
+    nodes.append(group2_node.worker_node(cfs, len(nodes), 3))
+
+
+    import group3_node
+    cfs = []
+    cfs.append(crazyflies[2])
+    nodes.append(group3_node.worker_node(cfs, len(nodes), 4))
+
+
+    import group4_node
+    cfs = []
+    cfs.append(crazyflies[3])
+    nodes.append(group4_node.worker_node(cfs, len(nodes), 5))
+
+
+    import group5_node
+    cfs = []
+    cfs.append(crazyflies[4])
+    nodes.append(group5_node.worker_node(cfs, len(nodes), 6))
+
+
+    import group6_node
+    cfs = []
+    cfs.append(crazyflies[5])
+    nodes.append(group6_node.worker_node(cfs, len(nodes), 7))
+
+
+    import group7_node
+    cfs = []
+    cfs.append(crazyflies[6])
+    nodes.append(group7_node.worker_node(cfs, len(nodes), 8))
+
+
+    import group8_node
+    cfs = []
+    cfs.append(crazyflies[7])
+    nodes.append(group8_node.worker_node(cfs, len(nodes), 9))
+
+
+
+    
+    # Launch all nodes
+    return launch(nodes)
+
+if __name__ == '__main__':
+    main()
